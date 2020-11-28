b803da3dbd85ea515eca74183f0d1c4a7a2b7c6f
Laurent Boeglen <laurent@kartludo.com>
2020-11-29 00:34:50 +0100

    Model + Faker + Routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,42 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/**
+**Basic Routes for a RESTful service:
+**Route::get($uri, $callback);
+**Route::post($uri, $callback);
+**Route::put($uri, $callback);
+**Route::delete($uri, $callback);
+**
+*/
+Route::get('products', function () {
+    return response(Product::all(),200);
+});
+ 
+Route::get('products/{product}', function ($productId) {
+    return response(Product::find($productId), 200);
+});
+  
+ 
+Route::post('products', function(Request $request) {
+   $resp = Product::create($request->all());
+    return $resp;
+ 
+});
+ 
+Route::put('products/{product}', function(Request $request, $productId) {
+    $product = Product::findOrFail($productId);
+    $product->update($request->all());
+    return $product;
+});
+ 
+Route::delete('products/{product}',function($productId) {
+    Product::find($productId)->delete();
+ 
+    return 204;
+ 
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
